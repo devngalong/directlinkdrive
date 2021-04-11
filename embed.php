fb18f38aa565b35da8764ff4b6ccc16753458d67
Dev ngalong <74237808+devngalong@users.noreply.github.com>
2021-04-11 14:37:55 +0700

    Update embed.php

--- embed.php
+++ embed.php
@@ -1,34 +1,30 @@
 <?php
-	include_once("drive.php");
-	if(isset($_GET['id'])){
-		$eid = htmlspecialchars($_GET['id']);
-		$results = file_get_contents(base_url.'json.php?id=https://drive.google.com/file/d/'.my_simple_crypt($eid, 'd').'/preview');
-		$results = json_decode($results, true);
-		// if($results['file']==1){
-	  //   echo '<center>Sorry, the owner hasn\'t given you permission to download this file.</center>';
-		// 	exit;
-	  // }elseif($results['file']==2) {
-		// 	echo '<center>Error 404. We\'re sorry. You can\'t access this item because it is in violation of our Terms of Service.</center>';
-		// 	exit;
-	  // }
-	}
+error_reporting(0);
+include "curl_gd.php";
+
+if($_GET['url'] != ""){
+	$url = $_GET['url'];
+	$linkdown = Drive($url);
+	$file = '[{"type": "video/mp4", "label": "HD", "file": "'.$linkdown.'"}]';
+}
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title><?php echo $results['title'];?></title>
-	<!-- Plyr.io Player -->
-	<link rel="stylesheet" href="https://cdn.plyr.io/3.3.12/plyr.css">
-</head>
-<body style="margin:0px;">
-	<video poster="<?php echo $results['image']; ?>" id="player" playsinline controls>
-		<source src="<?php echo $results['file'];?>" type="<?php echo $results['type'];?>">
-	</video>
-	<!-- Plyr JS -->
-	<script src="https://cdn.plyr.io/3.3.12/plyr.js"></script>
-	<script>const player = new Plyr('#player');</script>
-  </body>
-</html>
+<div id="myElement">
+		    
+		</div>
+		<script src="https://content.jwplatform.com/libraries/DbXZPMBQ.js"></script>
+	<script type="text/javascript">
+		jwplayer("myElement").setup({
+			playlist: [{
+				"sources":<?php echo $file?>
+			}],
+                skin: {
+       name: "stormtrooper",
+       active: "mediumpurple",
+       inactive: "#bcbcbc",
+       background: "rgba(65, 65, 65, 0.82)"
+    },
+			allowfullscreen: true,
+			width: '100%',
+			aspectratio: '16:9',
+		});
+	</script>
